db139aab56cbd645c8859cc502881553b8e0ffe3
Dan Nguyen Hai <nguyenhaidan13112000@gmail.com>
2023-03-17 20:02:20 +0700

    upload file

---
+++ app.js
@@ -0,0 +1,40 @@
+const menuItem = document.getElementById("menuItem");
+menuItem.style.maxHeight = "0px";
+function menutoggle() {
+  if (menuItem.style.maxHeight == "0px") {
+    menuItem.style.maxHeight = "200px";
+  } else {
+    menuItem.style.maxHeight = "0px";
+  }
+}
+/* slick slider */
+$(document).ready(function () {
+  $(".image-slider").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    infinite: true,
+    arrows: true,
+    draggable: false,
+    prevArrow: `<button type='button' class='slick-prev slick-arrow'><i class="fa-sharp fa-solid fa-arrow-left"></i></button>`,
+    nextArrow: `<button type='button' class='slick-next slick-arrow'><i class="fa-solid fa-arrow-right"></i></button>`,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          arrows: false,
+          infinite: false,
+        },
+      },
+    ],
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+});
